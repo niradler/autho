375d7ce77bd6483b18c46a4f788f19b988345eef
Nir Adler <me@niradler.com>
2024-04-13 02:23:56 +0300

    packages

---
+++ packages/models/Secret.js
@@ -0,0 +1,13 @@
+import Joi from 'joi';
+import { randomUUID } from 'node:crypto';
+
+
+export const createSecretSchema = Joi.object({
+  id: Joi.string().default(randomUUID()),
+  protected: Joi.boolean().default(false),
+  name: Joi.string().required(),
+  type: Joi.string().required().valid('otp', 'password', 'note'),
+  value: Joi.string().required(),
+  typeOptions: Joi.object().default({}),
+  createdAt: Joi.date().default(new Date()),
+});
---
+++ packages/shared/logger.js
@@ -0,0 +1,5 @@
+import { Logger as balogaLogger } from 'baloga';
+
+export const logger = new balogaLogger();
+
+export const Logger = balogaLogger;
